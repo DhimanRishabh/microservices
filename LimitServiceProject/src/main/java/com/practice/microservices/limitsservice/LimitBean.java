3480347cf1ca365ba7a2671e153704d075ecad4f
Rishabh <Rishabhdhiman321@gmail.com>
2019-06-19 18:04:46 +0530

    first commit

---
+++ LimitServiceProject/src/main/java/com/practice/microservices/limitsservice/LimitBean.java
@@ -0,0 +1,30 @@
+package com.practice.microservices.limitsservice;
+
+public class LimitBean {
+   int max;
+   int min;
+
+    public LimitBean() {
+    }
+
+    public LimitBean(int max, int min) {
+        this.max = max;
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+}
---
+++ LimitServiceProject/src/main/java/com/practice/microservices/limitsservice/LimitController.java
@@ -0,0 +1,18 @@
+package com.practice.microservices.limitsservice;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitController {
+
+    @Autowired
+    LimitConfiguration limitConfiguration;
+    @GetMapping("/getAll")
+    public LimitBean getAllLimits() {
+
+        return new LimitBean(limitConfiguration.getMin(),limitConfiguration.getMax());
+    }
+}
